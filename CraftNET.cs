937b136e07481a57fb129ed723a9a5c45b4562eb
akgamerz790 <rockdgyt352@gmail.com>
2026-01-16 00:53:53 +0530

    Initial Commit

---
+++ CraftNET.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CraftNET
+{
+    public class Bot
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
